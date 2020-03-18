c247d9e7dcfbe7401e22d43dfd617358091ed256
Adam <farfromruin@aol.com>
2020-03-18 11:36:01 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+
+
+function App() {
+  return <SearchResultContainer />;
+}
+
+export default App;
